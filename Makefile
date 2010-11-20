1f5f0b00c2a101c78f7dfdb5d05cf9133675da58
Satofumi KAMIMURA <satofumi@twinstar.(none)>
2010-11-20 16:03:03 +0900

    added chapter_01

--- Makefile
+++ Makefile
@@ -3,8 +3,16 @@
 HTML_DIR = generated_html
 
 
-all :
-	doxygen
+all : html
+	cd chapter_02/ && $(MAKE)
 
 clean :
 	$(RM) -rf $(HTML_DIR)
+	cd chapter_02/ && $(MAKE) clean
+
+html : $(HTML_DIR)/index.html
+
+.PHONY : all clean html
+######################################################################
+$(HTML_DIR)/index.html : Makefile Doxyfile $(wildcard dox/* chapter_01/* chapter_02/*)
+	doxygen
---
+++ chapter_02/example_2-1.cpp
@@ -0,0 +1,30 @@
+/*!
+  \example example_2-1.cpp 画像ファイルをスクリーンに表示するサンプル
+*/
+
+#include "highgui.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 1) {
+        cout << "usage: " << argv[0] << " <image file>" << endl;
+        return 1;
+    }
+
+    const char* image_file = argv[1];
+    IplImage* img = cvLoadImage(image_file);
+
+    const char* window_title = "Example 1";
+    cvNamedWindow(window_title, CV_WINDOW_AUTOSIZE);
+    cvShowImage(window_title, img);
+    cvWaitKey(0);
+
+    cvReleaseImage(&img);
+    cvDestroyWindow(window_title);
+
+    return 0;
+}
