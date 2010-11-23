5897889b06290242beb2bd65ce9e98717ac70c45
Satofumi KAMIMURA <satofumi@twinstar.(none)>
2010-11-23 13:26:58 +0900

    write example_2-5.cpp article

--- chapter_02/example_2-5.cpp
+++ chapter_02/example_2-5.cpp
@@ -1,14 +1,55 @@
 /*!
-  \brief example_2-5.cpp 
+  \brief example_2-5.cpp 画像サイズの変更
 
   \author Satofumi KAMIMURA
 
   $Id$
 */
 
+#include "cv.h"
+#include "highgui.h"
+#include <iostream>
+
+using namespace std;
+
+
+namespace
+{
+    IplImage* do_pyr_down(IplImage* input)
+    {
+#if 0
+        assert(((input->width % 2) == 0) &&
+               ((input->height % 2) == 0));
+#endif
+
+        IplImage* output = cvCreateImage(cvSize(input->width / 2,
+                                                input->height / 2),
+                                         input->depth,
+                                         input->nChannels);
+        cvPyrDown(input, output);
+        return output;
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
-    // !!!
+    if (argc <= 1) {
+        cout << "usage: " << argv[0] << " <image file>" << endl;
+        return 1;
+    }
+
+    const char* image_file = argv[1];
+    IplImage* original = cvLoadImage(image_file);
+    IplImage* converted = do_pyr_down(original);
+
+    const char* window_name = "Example 5";
+    cvNamedWindow(window_name);
+    cvShowImage(window_name, converted);
+
+    // キーの入力待ち
+    cvWaitKey(0);
+    cvDestroyWindow(window_name);
+
     return 0;
 }
---
+++ chapter_02/example_2-6.cpp
@@ -0,0 +1,14 @@
+/*!
+  \brief example_2-6.cpp Canny エッジ検出フィルタの適用
+
+  \author Satofumi KAMIMURA
+
+  $Id$
+*/
+
+
+int main(int argc, char *argv[])
+{
+    // !!!
+    return 0;
+}
