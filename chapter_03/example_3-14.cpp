323d0567bf499fcf89aa521869b8a735c45d13fd
Satofumi KAMIMURA <satofumi@twinstar.(none)>
2011-01-21 05:33:12 +0900

    added chapter 3 samples

--- chapter_03/example_3-14.cpp
+++ chapter_03/example_3-14.cpp
@@ -6,10 +6,33 @@
   $Id$
 */
 
-// !!!
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
 
 
 int main(int argc, char *argv[])
 {
-    // !!!
+    IplImage* src1 = NULL;
+    IplImage* src2 = NULL;
+
+    if ((argc == 9) && ((src1 = cvLoadImage(argv[1], 1)) != 0) &&
+        ((src2 = cvLoadImage(argv[2], 1)) != 0)) {
+        int x = atoi(argv[3]);
+        int y = atoi(argv[4]);
+        int width = atoi(argv[5]);
+        int height = atoi(argv[6]);
+
+        double alpha = (double)atof(argv[7]);
+        double beta = (double)atof(argv[8]);
+        cvSetImageROI(src1, cvRect(x, y, width, height));
+        cvSetImageROI(src2, cvRect(0, 0, width, height));
+        cvAddWeighted(src1, alpha, src2, beta, 0.0, src1);
+        cvResetImageROI(src1);
+
+        cvNamedWindow("Alpha_blend", 1);
+        cvShowImage("Alpha_blend", src1);
+        cvWaitKey();
+    }
+
+    return 0;
 }
--- chapter_03/example_3-19.cpp
+++ chapter_03/example_3-19.cpp
@@ -1,5 +1,5 @@
 /*!
-  \example example_3-19.cpp 構造体の設定を読み書きする例
+  \example example_3-19.cpp 構造体の設定を読み込む例
 
   \author Satofumi KAMIMURA
 
@@ -22,16 +22,27 @@ namespace
 
         cout << "frame_count: " << frame_count << endl;
 
-        // !!!
-#if 0
-        cvWriteInt(fs, "frame_count", 10);
-        cvStartWriteStruct(fs, "frame_size", CV_NODE_SEQ);
-        cvWriteInt(fs, 0, 320);
-        cvWriteInt(fs, 0, 200);
-        cvEndWriteStruct(fs);
-        cvWrite(fs, "color_cvt_matrix", cmatrix);
+        CvSeq* s = cvGetFileNodeByName(fs, 0, "frame_size")->data.seq;
+        int frame_width = cvReadInt((CvFileNode*)cvGetSeqElem(s, 0));
+        int frame_height = cvReadInt((CvFileNode*)cvGetSeqElem(s, 1));
+        cout << "frame_width: " << frame_width << endl
+             << "frame_height: " << frame_height << endl;
+
+        CvMat* color_cvt_matrix =
+            (CvMat*)cvReadByName(fs, 0, "color_cvt_matrix");
+
+        int rows = color_cvt_matrix->rows;
+        int cols = color_cvt_matrix->cols;
+        cout << "matrix_rows: " << rows << endl
+             << "matrix_cols: " << cols << endl;
+
+        for (int y = 0; y < rows; ++y) {
+            for (int x = 0; x < cols; ++x) {
+                cout << cvmGet(color_cvt_matrix, x, y) << endl;
+            }
+        }
+
         cvReleaseFileStorage(&fs);
-#endif
     }
 }
 
